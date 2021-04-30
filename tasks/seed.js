0e72efc9bf99c23a62a40b2520bd5ed33d01d11f
Ronak kachalia <r.kachalia.20@gmail.com>
2021-04-30 15:34:26 -0400

    Add users, schools databse seed

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,15 +1,49 @@
-const dbConnection = require("../config/mongoConnection");
-const data = require("../data/");
+const mongodbConnection = require("../config/mongoConnection");
+const schoolService = require('../services').schoolService;
+const userService = require('../services').userService;
 
 (async () => {
   try {
-    const db = await dbConnection();
+    const db = await mongodbConnection.getDB();
     await db.dropDatabase();
+    console.log("Database dropped");
+    console.log("Starting database seed");
 
-    // TODO - logic for seeding DB
+    let result = null;
 
+    // Users Collection
+    // Add Users
+    let users = [
+      ["Ronak", "Kachalia", "Password", "rkachali@stevens.edu"],
+      ["Sam", "Severance", "Password", "sam@stevens.edu"],
+      ["Matt", "Wisnewski", "Password", "matt@stevens.edu"],
+      ["Karan", "Shah", "Password", "kshah@stevens.edu"]
+    ];
+    users.forEach(async (user) => {
+      result = await userService.addUser(...user, new Date());
+      if(result.isInserted) {
+        console.log(`User - ${user[0]} ${user[1]} added successfully`);
+        user.push(result.userId);
+      }
+      else console.log(`User - ${user[0]} ${user[1]} could not be added`);
+    });
+
+    // Schools Collection
+    // Add Schools
+    let schools = [
+      ["Stevens Institute of Technology", "College", "Hoboken", "New Jersey", "07030", users[0][4]],
+      ["NJIT", "College", "Newark", "New Jersey", "07102", users[1][4]],
+      ["NYU", "College", "New York City", "New York", "10003", users[2][4]],
+      ["Rutgers University", "New Brunswick", "New Jersey", "08901", users[3][4]]
+    ];
+
+    schools.forEach(async (school) => {
+      result = await schoolService.addSchool(...school);
+      if(result) console.log(`School - ${school[0]} added successfully`);
+      else console.log(`School - ${school[0]} could not be added`);
+    });
     console.log("Done seeding database");
-    await db.serverConfig.close();
+    // await db.serverConfig.close();
   } catch (e) {
     console.error(e);
   }
