afbeb627790c997671013408145fad5cd8b8880c
Sam Severance <swseverance@gmail.com>
2021-05-03 19:12:45 -0400

    small changes to `seed.js`

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,13 +1,15 @@
+const chalk = require("chalk");
+
 const mongodbConnection = require("../config/mongoConnection");
-const schoolService = require('../services').schoolService;
-const userService = require('../services').userService;
+const schoolService = require("../services").schoolService;
+const userService = require("../services").userService;
 
 (async () => {
   try {
     const db = await mongodbConnection.getDB();
     await db.dropDatabase();
-    console.log("Database dropped");
-    console.log("Starting database seed");
+    console.log(chalk.blue("Database dropped"));
+    console.log(chalk.blue("Starting database seed"));
 
     let result = null;
 
@@ -17,33 +19,52 @@ const userService = require('../services').userService;
       ["Ronak", "Kachalia", "Password", "rkachali@stevens.edu"],
       ["Sam", "Severance", "Password", "sam@stevens.edu"],
       ["Matt", "Wisnewski", "Password", "matt@stevens.edu"],
-      ["Karan", "Shah", "Password", "kshah@stevens.edu"]
+      ["Karan", "Shah", "Password", "kshah@stevens.edu"],
     ];
-    users.forEach(async (user) => {
+    for (const user of users) {
       result = await userService.addUser(...user, new Date());
-      if(result.isInserted) {
-        console.log(`User - ${user[0]} ${user[1]} added successfully`);
+      if (result.isInserted) {
+        console.log(
+          chalk.green(`User - ${user[0]} ${user[1]} added successfully`)
+        );
         user.push(result.userId);
-      }
-      else console.log(`User - ${user[0]} ${user[1]} could not be added`);
-    });
+      } else
+        console.error(
+          chalk.red(`User - ${user[0]} ${user[1]} could not be added`)
+        );
+    }
 
     // Schools Collection
     // Add Schools
     let schools = [
-      ["Stevens Institute of Technology", "College", "Hoboken", "New Jersey", "07030", users[0][4]],
+      [
+        "Stevens Institute of Technology",
+        "College",
+        "Hoboken",
+        "New Jersey",
+        "07030",
+        users[0][4],
+      ],
       ["NJIT", "College", "Newark", "New Jersey", "07102", users[1][4]],
       ["NYU", "College", "New York City", "New York", "10003", users[2][4]],
-      ["Rutgers University", "New Brunswick", "New Jersey", "08901", users[3][4]]
+      [
+        "Rutgers University",
+        "College",
+        "New Brunswick",
+        "New Jersey",
+        "08901",
+        users[3][4],
+      ],
     ];
 
-    schools.forEach(async (school) => {
+    for (const school of schools) {
       result = await schoolService.addSchool(...school);
-      if(result) console.log(`School - ${school[0]} added successfully`);
-      else console.log(`School - ${school[0]} could not be added`);
-    });
-    console.log("Done seeding database");
-    // await db.serverConfig.close();
+      if (result)
+        console.log(chalk.green(`School - ${school[0]} added successfully`));
+      else console.error(chalk.red(`School - ${school[0]} could not be added`));
+    }
+    console.log(chalk.blue("Done seeding database"));
+    await db.serverConfig.close();
   } catch (e) {
     console.error(e);
   }
